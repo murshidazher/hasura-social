c21b332d1e93194eacece1072638ccf5b174c210
murshidazher <murshidazher@yahoo.com>
2021-01-01 15:43:34 +0530

    feat: add initial apollo provider

--- react-hasura-app/src/App.tsx
+++ react-hasura-app/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  GraphQLRequest,
+  InMemoryCache,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080/v1/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <h1>Hello World!</h1>
+    </ApolloProvider>
   );
 }
 
